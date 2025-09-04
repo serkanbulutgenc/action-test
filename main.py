2508075a9f96b544d80c50e9176b0e81fc011549
Serkan <serkanbulutgenc@gmail.com>
2025-09-04 19:38:53 +0300

    Added linter actions

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from typing import Sequence
 import datetime
 
 
+def f(x):
+    '''
+    Something about `f`. And an example:
 
+    .. code-block:: python
+
+        foo, bar, quux = this_is_a_long_line(lion, hippo, lemur, bear)
+    '''
+    pass
 
 
 def main()->None:
